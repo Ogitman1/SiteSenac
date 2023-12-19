4ddc83adb3044b658b6018822243d70f3681cf29
Matheus Santos <matheussfelipe.santos@gmail.com>
2023-12-19 19:11:15 -0300

    Nova foto do Ale e dropdown

--- Form.js
+++ Form.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get form, submit button, and required fields
+    // Pega o id de form, submit button, e um array de 'ids'
     const form = document.getElementById('myForm');
     const submitBtn = document.getElementById('submitBtn');
     const requiredFields = ['name', 'email'];
-
+    //Evento de input checa se todos os campos foram preenchidos
     form.addEventListener('input', function(){
         const AllFieldsRequired = requiredFields.every(fieldName => {
         const element = document.getElementById(fieldName);
         return element.value.trim() !== "";
     })
+    //Se os campos forem preenchidos o disabled irá ser falso e vice-versa
     submitBtn.disabled = !AllFieldsRequired;
     })
-    
+    //Se o disabled for falso aparecerá um alert falando que o formulário foi enviado
     form.addEventListener('submit', function (event) {
         if(!submitBtn.disabled){
             alert('Formulário enviado!');
---
+++ Mouse.js
@@ -0,0 +1,17 @@
+function Mouse() {
+    const links = document.getElementsByClassName("dropdown-item");
+
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener('mouseover', function () {
+            this.classList.add('active'); // 'this' se refere ao elemento específico que acionou o evento
+            console.log('active');
+        });
+
+        links[i].addEventListener('mouseout', function () {
+            this.classList.remove('active'); // 'this' se refere ao elemento específico que acionou o evento
+        });
+    }
+}
+
+// Chama a função quando a página é carregada
+window.onload = Mouse;
