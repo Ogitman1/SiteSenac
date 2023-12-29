0d0da3813dd3059e691bab85970ce2519459d9e3
Matheus Santos <matheussfelipe.santos@gmail.com>
2023-12-29 10:58:03 -0300

    Novas imagens do senac, troca de imagens do carrossel e campo de busca

--- Mouseover.js
+++ Mouseover.js
@@ -1,6 +1,6 @@
 function Mouse() {
     const links = document.getElementsByClassName("dropdown-item");
-
+    
     for (let i = 0; i < links.length; i++) {
         links[i].addEventListener('mouseover', function () {
             this.classList.add('active'); // 'this' se refere ao elemento específico que acionou o evento
